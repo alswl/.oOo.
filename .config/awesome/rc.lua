7887853844bdc352ae2e610ea100633c1ea9c8a6
alswl <alswlx@gmail.com>
2012-09-08 23:30:06 +0800

    fix default terminal bug

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -42,7 +42,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "gnome-terminal"
+terminal = "tilda"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
