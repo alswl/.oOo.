1f0dca19ebbc4ca98b6ad4c904cd14a0901632c2
alswl <alswlx@gmail.com>
2012-09-18 23:50:39 +0800

    localize vary

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -160,13 +160,13 @@ local batwidget1 = widget({ type = "textbox" })
 vicious.register(batwidget1, vicious.widgets.bat, ' <span color="#0000ff">$1$2%</span>', 5, 'BAT0')
 
 -- Create a systray
-mysystray = widget({ type = "systray" })
+local mysystray = widget({ type = "systray" })
 
 -- Create a wibox for each screen and add it
-mywibox = {}
-mypromptbox = {}
-mylayoutbox = {}
-mytaglist = {}
+local mywibox = {}
+local mypromptbox = {}
+local mylayoutbox = {}
+local mytaglist = {}
 -- mouse control tag list bar
 mytaglist.buttons = awful.util.table.join(
     awful.button({ }, 1, awful.tag.viewonly),
@@ -176,8 +176,8 @@ mytaglist.buttons = awful.util.table.join(
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 )
-mytasklist = { }
-mystatuslist = { }
+local mytasklist = { }
+local mystatuslist = { }
 mytasklist.buttons = awful.util.table.join(
     awful.button({}, 1,
         function (c)
@@ -217,7 +217,8 @@ mytasklist.buttons = awful.util.table.join(
 for s = 1, screen.count() do
     -- Create a promptbox for each screen
     mypromptbox[s] = awful.widget.prompt({
-        layout = awful.widget.layout.horizontal.leftright})
+        layout = awful.widget.layout.horizontal.leftright
+    })
     -- Create an imagebox widget which will contains an icon indicating which layout we're using.
     -- We need one layoutbox per screen.
     mylayoutbox[s] = awful.widget.layoutbox(s)
