83dfb7fdc8592237fb299d82db23c0551f3070e4
alswl <alswlx@gmail.com>
2015-09-06 13:42:39 +0800

    support dash Cheat Shets / User Contributed

--- local/bin/generate_dash_index.sh
+++ local/bin/generate_dash_index.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-FINAL_INDEX_PATH="$HOME/Library/Application Support/Dash/DocSets/index.html"
-TEMP_INDEX_PATH="$HOME/Library/Application Support/Dash/DocSets/index.html.tmp"
-BODY_INDEX_PATH="$HOME/Library/Application Support/Dash/DocSets/index.html.body"
+FINAL_INDEX_PATH="$HOME/Library/Application Support/Dash/index.html"
+TEMP_INDEX_PATH="$HOME/Library/Application Support/Dash/index.html.tmp"
+BODY_INDEX_PATH="$HOME/Library/Application Support/Dash/index.html.body"
 WHOAMI=`whoami`
 
 echo -n '' > "$TEMP_INDEX_PATH"
 
-ls $HOME/Library/Application\ Support/Dash/DocSets/*/*/Contents/Resources/Documents/index.html | sed "s/\\/Users\\/$WHOAMI\\/Library\\/Application Support\\/Dash\\/DocSets\\///g" >> "$TEMP_INDEX_PATH"
-ls $HOME/Library/Application\ Support/Dash/DocSets/*/*/Contents/Resources/Documents/doc/index.html | sed "s/\\/Users\\/$WHOAMI\\/Library\\/Application Support\\/Dash\\/DocSets\\///g" >> "$TEMP_INDEX_PATH"
+ls $HOME/Library/Application\ Support/Dash/DocSets/*/*/Contents/Resources/Documents/index.html | sed "s/\\/Users\\/$WHOAMI\\/Library\\/Application Support\\/Dash\\///g" >> "$TEMP_INDEX_PATH"
+ls $HOME/Library/Application\ Support/Dash/DocSets/*/*/Contents/Resources/Documents/doc/index.html | sed "s/\\/Users\\/$WHOAMI\\/Library\\/Application Support\\/Dash\\///g" >> "$TEMP_INDEX_PATH"
+
+ls $HOME/Library/Application\ Support/Dash/User\ Contributed/*/*/Contents/Resources/Documents/index.html | sed "s/\\/Users\\/$WHOAMI\\/Library\\/Application Support\\/Dash\\///g" >> "$TEMP_INDEX_PATH"
+ls $HOME/Library/Application\ Support/Dash/User\ Contributed/*/*/Contents/Resources/Documents/configuration.html | sed "s/\\/Users\\/$WHOAMI\\/Library\\/Application Support\\/Dash\\///g" >> "$TEMP_INDEX_PATH"
+
+ls $HOME/Library/Application\ Support/Dash/Cheat\ Sheets/*/*/Contents/Resources/Documents/index.html | sed "s/\\/Users\\/$WHOAMI\\/Library\\/Application Support\\/Dash\\///g" >> "$TEMP_INDEX_PATH"
 
 
-awk -F '/' '{printf("<li><a href=\"%s\">%s</a></li>", $0, $1);}' "$TEMP_INDEX_PATH" > "$BODY_INDEX_PATH"
+awk -F '/' '{printf("<li><a href=\"%s\">%s</a></li>", $0, $2);}' "$TEMP_INDEX_PATH" > "$BODY_INDEX_PATH"
 
 echo -n '' > "$FINAL_INDEX_PATH"
 echo '<html>' >> "$FINAL_INDEX_PATH"
