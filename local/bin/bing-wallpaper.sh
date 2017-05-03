255e6c99444bea4635efd787f47336dec992fecf
alswl <alswlx@gmail.com>
2017-05-03 11:59:01 +0800

    feat: locale

--- local/bin/bing-wallpaper.sh
+++ local/bin/bing-wallpaper.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
+LOCALE=zh-CN
 mkdir -p $PICTURE_DIR
 
-URL="http://s.cn.bing.net"`http 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US' | jq -r '.images[0].url'`
+URL="http://s.cn.bing.net"`http "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$LOCALE" | jq -r '.images[0].url'`
 #echo $URL
 wget -q -nc -P $PICTURE_DIR $URL
