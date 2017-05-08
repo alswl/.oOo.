2bc2442e9a3e041ca6b1e048321a6adedfeb385b
alswl <alswlx@gmail.com>
2017-05-08 11:04:28 +0800

    fix: latest 7 day

--- local/bin/bing-wallpaper.sh
+++ local/bin/bing-wallpaper.sh
@@ -4,6 +4,7 @@ PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 LOCALE=zh-CN
 mkdir -p $PICTURE_DIR
 
-URL="http://s.cn.bing.net"`http "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$LOCALE" | jq -r '.images[0].url'`
-#echo $URL
-wget -q -nc -P $PICTURE_DIR $URL
+for i in {0..7}; do
+	URL="http://s.cn.bing.net"`curl -s "http://www.bing.com/HPImageArchive.aspx?format=js&idx=$i&n=1&mkt=$LOCALE" | /usr/local/bin/jq -r '.images[0].url'`
+	wget -q -nc -P $PICTURE_DIR $URL
+done
