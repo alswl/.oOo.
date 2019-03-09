f8fcd81b6b45d05b35e4f673bd6427077e1bc012
alswl <alswlx@gmail.com>
2019-03-09 10:26:14 +0800

    fix: new bing api

--- local/bin/bing-wallpaper.sh
+++ local/bin/bing-wallpaper.sh
@@ -8,5 +8,6 @@ mkdir -p $PICTURE_DIR
 
 for i in {0..7}; do
 	URL="http://s.cn.bing.net"`curl -s "http://www.bing.com/HPImageArchive.aspx?format=js&idx=$i&n=1&mkt=$LOCALE" | jq -r '.images[0].url'`
-	wget -q -nc -P $PICTURE_DIR $URL
+	FILENAME=$(echo $URL | ggrep -oP '\K(id=[^=]+.jpg)' | gsed 's/id=//g' | gsed 's/OHR\.//g')
+	wget -q -nc -P $PICTURE_DIR -O $FILENAME $URL
 done
