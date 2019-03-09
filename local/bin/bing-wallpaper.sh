b58d191e88745c1c09192e7fe4dea49e441e7b99
alswl <alswlx@gmail.com>
2019-03-09 11:48:06 +0800

    fix: .

--- local/bin/bing-wallpaper.sh
+++ local/bin/bing-wallpaper.sh
@@ -2,12 +2,12 @@
 
 export PATH=$PATH:/usr/local/bin
 
-PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
+PICTURE_DIR="$HOME/Pictures/bing-wallpapers"
 LOCALE=zh-CN
 mkdir -p $PICTURE_DIR
 
 for i in {0..7}; do
 	URL="http://s.cn.bing.net"`curl -s "http://www.bing.com/HPImageArchive.aspx?format=js&idx=$i&n=1&mkt=$LOCALE" | jq -r '.images[0].url'`
 	FILENAME=$(echo $URL | ggrep -oP '\K(id=[^=]+.jpg)' | gsed 's/id=//g' | gsed 's/OHR\.//g')
-	wget -q -nc -P $PICTURE_DIR -O $FILENAME $URL
+	wget -q -nc -O $PICTURE_DIR/$FILENAME $URL
 done
